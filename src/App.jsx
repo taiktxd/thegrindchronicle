91d4700c4c7288484ab03491df12dd0f14ddb693
taiktxd <taiktxd@gmail.com>
2026-07-05 13:05:41 +0700

    fix: switch to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { HashRouter, Routes, Route } from "react-router-dom"; // Đổi ở đây
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import PostDetail from "./pages/PostDetail";
 
 function App() {
   return (
-    <HashRouter> {/* Đổi ở đây */}
+    <HashRouter>
       <Header />
+
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/post/:slug" element={<PostDetail />} />
         </Routes>
       </main>
+
       <Footer />
-    </HashRouter> {/* Đổi ở đây */}
+    </HashRouter>
   );
 }
+
+export default App;
