ab5043483f75eea23e32a49263f65448738edafe
taiktxd <taiktxd@gmail.com>
2026-07-07 11:36:19 +0700

    Fix base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/thegrindchronicle/',
+  base: '/',           // Phải là '/' khi deploy Vercel
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
