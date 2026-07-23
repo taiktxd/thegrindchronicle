7b58c61b2aa4e16954696b31d45431ec3820e7ac
taiktxd <taiktxd@gmail.com>
2026-07-24 02:12:57 +0700

    fix site 100

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 Đổi từ '/' thành './' để chạy được trên CẢ Vercel LẪN GitHub Pages
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  base: '/', // Vercel BẮT BUỘC phải là /
 })
